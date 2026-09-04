c76b6f731b4672b242271559d74341753fdb6bd2
Alex Glassford <abglassford@icloud.com>
2026-09-04 17:33:29 -0600

    send actual info

--- src/main/java/com/sup/Controller.java
+++ src/main/java/com/sup/Controller.java
@@ -1,5 +1,7 @@
 package com.sup;
 
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class Controller {
 
+    private final SystemInfo sysInfo;
+
+    public Controller(SystemInfo sysInfo) {
+        this.sysInfo = sysInfo;
+    }
+
     @GetMapping("/info")
-    public String getStuff() {
-        return "Stuff!!!";
+    public String getStuff() throws IOException {
+        return sysInfo.getInfo();
     }
 }
--- src/main/java/com/sup/SystemInfo.java
+++ src/main/java/com/sup/SystemInfo.java
@@ -10,45 +10,60 @@ import org.springframework.stereotype.Component;
 @Component
 public class SystemInfo {
 
-    public void getInfo() throws IOException {
+    private String info;
+
+    public String getInfo() throws IOException {
         System.out.println("Hello World!");
 
         try {
             System.out.println(Instant.now() + "\n");
             // TODO: Parse system info better for better logging
-            runCommand("cat", "/etc/os-release");
-            runCommand("cat", "/proc/stat");
-            runCommand("cat", "/proc/meminfo");
-            runCommand("cat", "/sys/class/thermal/thermal_zone0/temp");
+            StringBuilder finalOutput = new StringBuilder();
+
+            finalOutput
+                    .append(runCommand("cat", "/etc/os-release")).append(System.lineSeparator())
+                    .append(runCommand("cat", "/proc/stat")).append(System.lineSeparator())
+                    .append(runCommand("cat", "/proc/meminfo")).append(System.lineSeparator());
+
+            this.info = finalOutput.toString().trim();
+
+            return this.info;
+
         } catch (Exception e) {
             e.printStackTrace();
+            return "";
         }
     }
 
-    public void runCommand(String... args) throws IOException {
+    public String runCommand(String... args) throws IOException {
         System.out.println("Running command \"" + String.join(" ", args) + "\"");
         ProcessBuilder pb = new ProcessBuilder(args);
         pb.redirectErrorStream(true);
 
         Process process = pb.start();
 
+        StringBuilder output = new StringBuilder();
+
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
             String line;
 
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                output.append(line).append(System.lineSeparator());
             }
 
-            System.out.println("\n");
             int exitCode = process.waitFor();
 
             if (exitCode != 0) {
                 System.err.println("Command failed with exit code: " + exitCode);
             }
 
+            return output.toString().trim();
+
         } catch (Exception e) {
             e.printStackTrace();
+            return "";
         }
+
 
     }
 }
